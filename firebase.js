3dec410cd677bebf325a46803e6bc54aefc493f4
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 22:00:39 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcXkVFQzB2XtxO7wqnbXhzM1Io54zCsBI",
@@ -10,7 +9,6 @@ const firebaseConfig = {
   storageBucket: "fiyattakip-ttoxub.firebasestorage.app",
   messagingSenderId: "105868725844",
   appId: "1:105868725844:web:fc04f5a08e708916e727c1",
-  measurementId: "G-M6JXDZ3PK0"
 };
 
 export const app = initializeApp(firebaseConfig);
