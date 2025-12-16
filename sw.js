09830c63270ede9dd5cae95eaf95a30d083963ce
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 22:04:45 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -14,8 +14,14 @@ self.addEventListener("install", (e)=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
+  );
+});
+
 self.addEventListener("fetch", (e)=>{
   e.respondWith(
-    caches.match(e.request).then(r=> r || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
