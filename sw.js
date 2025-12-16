1e0c99536f09570094b64b50941929d706d98ef8
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 18:44:49 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ const CACHE = "fiyattakip-v1";
 const ASSETS = [
   "./",
   "./index.html",
+  "./app.js",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
@@ -9,21 +10,20 @@ const ASSETS = [
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  const req = e.request;
   e.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE).then((c) => c.put(req, copy)).catch(()=>{});
-      return res;
-    }).catch(() => caches.match("./index.html")))
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
   );
 });
