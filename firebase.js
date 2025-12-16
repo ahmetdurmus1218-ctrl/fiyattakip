e3b2696949ec5254dedd9e0b169f705d0f9d711e
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 20:25:09 +0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,56 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  collection,
+  addDoc,
+  deleteDoc,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  serverTimestamp,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcXkVFQzB2XtxO7wqnbXhzM1Io54zCsBI",
+  authDomain: "fiyattakip-ttoxub.firebaseapp.com",
+  projectId: "fiyattakip-ttoxub",
+  storageBucket: "fiyattakip-ttoxub.firebasestorage.app",
+  messagingSenderId: "105868725844",
+  appId: "1:105868725844:web:fc04f5a08e708916e727c1",
+  measurementId: "G-M6JXDZ3PK0"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const fb = {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  signOut,
+
+  doc, setDoc, getDoc,
+  collection, addDoc, deleteDoc,
+  getDocs, query, where, orderBy, limit,
+  serverTimestamp, updateDoc
+};
