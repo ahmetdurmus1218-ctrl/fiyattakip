5afed90b368a0da8a57abd90f5c0b199ff5c15d8
ahmetdurmus1218-ctrl <ahmetdurmus1218@gmail.com>
2025-12-16 14:09:14 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,25 @@
-const CACHE_NAME = "fiyattakip-v1";
-const urlsToCache = [
+const CACHE = "fiyattakip-v2";
+const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null))))
   );
+  self.clients.claim();
 });
 
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match("./")))
+  );
+});
